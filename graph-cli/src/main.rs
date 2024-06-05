ceede79093df026403d97de031051e338d0de2eb
paullj <paullj1998@gmail.com>
2024-06-06 00:14:21 +0100

    feat: add open in browser to cli

--- graph-cli/src/main.rs
+++ graph-cli/src/main.rs
@@ -18,6 +18,9 @@ struct Arguments {
     /// The file format of the output file
     #[arg(value_enum, short, long, default_value_t = OutputFormat::Svg)]
     format: OutputFormat,
+    /// Open the output file in the default browser
+    #[arg(short = 'b', long, default_value_t = false)]
+    open: bool,
 }
 
 // TODO: Move this into core?
@@ -37,23 +40,38 @@ fn main() -> Result<()> {
     let test = graph_core::generate_graph(&content);
     match test {
         Ok(graph) => {
-            // Create a temporary file
-            let temp_file = Builder::new()
-                .suffix(".svg")
-                .tempfile()
-                .with_context(|| "Could not create temporary file")?;
-            // Write the graph to the temporary file
-            fs::write(temp_file.path(), graph)?;
-            // Open the temporary file in the browser
-            let output_path = temp_file
-                .path()
-                .to_str()
-                .with_context(|| "Could not convert path to string")?;
-            // Open the file in the browser
-            let _ = webbrowser::open(output_path)
-                .with_context(|| "Could not open graph with browser")?;
-            // wait for 1s to allow the browser to open
-            std::thread::sleep(std::time::Duration::from_secs(1));
+            let clone = graph.clone();
+            if let Some(output_path) = &args.output_path {
+                fs::write(output_path, clone).with_context(|| {
+                    format!(
+                        "Could not write to output file at `{}`",
+                        output_path.display()
+                    )
+                })?;
+            } else {
+                // TODO: Print the graph to the console (with formatting?)
+                // println!("{}", clone);
+            }
+            // Open the output file in the browser
+            if args.open {
+                // Create a temporary file
+                let temp_file = Builder::new()
+                    .suffix(".svg")
+                    .tempfile()
+                    .with_context(|| "Could not create temporary file")?;
+                // Write the graph to the temporary file
+                fs::write(temp_file.path(), graph)?;
+                // Open the temporary file in the browser
+                let output_path = temp_file
+                    .path()
+                    .to_str()
+                    .with_context(|| "Could not convert path to string")?;
+                // Open the file in the browser
+                let _ = webbrowser::open(output_path)
+                    .with_context(|| "Could not open graph with browser")?;
+                // wait for 1s to allow the browser to open
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
         }
         Err(error) => eprintln!("{error}"),
     }
